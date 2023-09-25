657f27e0b1a8def7afb31989e360a4c515164032
Andrei Stupakou <kfx0000@gmail.com>
2023-09-25 21:38:17 +0300

    feat: add side panels for flags and timer

--- random-game/script.js
+++ random-game/script.js
@@ -1,1 +1,11 @@
 // This is not a classic Miner game. To win, you have to place all the flags on the mined tiles. When you set the last flag, if any of the flags are set wrong, you'll lose.
+
+newGame();
+
+
+function newGame() {
+}
+
+
+function showMenu() {
+}
