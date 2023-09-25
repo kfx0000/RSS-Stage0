b20847a6e67fc60c0b64e0f76b96fa77828b9def
Andrei Stupakou <kfx0000@gmail.com>
2023-09-25 22:10:23 +0300

    feat: add settings menu

--- random-game/script.js
+++ random-game/script.js
@@ -8,4 +8,21 @@ function newGame() {
 
 
 function showMenu() {
+    if(document.querySelector(".menu").classList.contains("menu_show")) {
+        document.querySelector(".menu").classList.remove("menu_show");
+    } else {
+        document.querySelector(".menu").classList.add("menu_show");
+    }
 }
+
+
+document.addEventListener("click", (event) => {
+    if(!event.target.classList.contains("menu") &&
+        !event.target.classList.contains("header__butt-container") &&
+        !event.target.classList.contains("header__button") &&
+        !event.target.classList.contains("menu__caption") &&
+        !event.target.classList.contains("menu__selector") &&
+        !event.target.classList.contains("menu__selector-capt"))
+            document.querySelector(".menu").classList.remove("menu_show");
+    console.log(event.target.classList);
+});
