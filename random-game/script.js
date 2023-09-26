45498686cb1cf6c9f6d3ee5ca17174fcbde4fd37
Andrei Stupakou <kfx0000@gmail.com>
2023-09-26 06:31:17 +0300

    feat: add gameplay functionality

--- random-game/script.js
+++ random-game/script.js
@@ -28,10 +28,7 @@ function newGame() {
         for(let i = 0; i < 9; i++) arr[i] = new Array(10).fill({name:'', time:0, date: ''});
         localStorage.setItem("RSS_Miner", JSON.stringify(arr));
     }
-    const elements = document.getElementsByClassName("tile");
-    while(elements.length > 0){
-        elements[0].parentNode.removeChild(elements[0]);
-    }
+    document.querySelectorAll(".tile").forEach((x) => x.remove());
     const board = document.getElementById("board");
     for(let i = 1; i <= numCells; i++) {
         let tile = document.createElement("div");
@@ -84,6 +81,121 @@ function showMenu() {
 }
 
 
+
+function updateTimer() {
+    ++currTime;
+    document.querySelector(".timer").textContent = getTime(currTime);
+    if(currTime >= timeLimit) openBombs(0,"Time is up!!!", "#44e");
+}
+
+function getTime(t) {
+    let ms = t%10;
+    let sec = ((t-t%10)/10)%60;
+    sec = sec < 10 ? '0'+sec : ''+sec;
+    let min = Math.floor((t/10 - (((t-t%10)/10)%60))/60);
+    min = min < 10 ? '0'+min : ''+min;
+    return min + ':' + sec + '.' + ms;
+}
+
+function openTile(num) {
+    if(firstClick) {
+        firstClick = false;
+        timer = setInterval(updateTimer, 100);
+    }
+    if(!document.querySelector(`.${num}`).classList.contains("tile_flag")) {
+        let cell = +num.split('-')[1];
+        if(boardArray[cell]) {
+            openBomb(num, "red");
+            openBombs(cell);
+        } else {
+            document.querySelector(`.${num}`).classList.add("tile_open");
+            countTile(cell);
+        }
+    }
+}
+
+function openBomb(num, color) {
+    let bombTile = document.createElement("div");
+    bombTile.classList.add(`bomb-${color}`);
+    document.querySelector(`.${num}`).classList.add("tile_open");
+    document.getElementById(num).appendChild(bombTile);
+}
+
+function openBombs(cell) {
+    for(let i = 1; i <= numCells; i++) {
+        let tile = document.getElementById(`tile-${i}`).classList;
+        if((i !== cell) && boardArray[i] && (!tile.contains("tile_flag")))
+            openBomb(`tile-${i}`, "black");
+        if((tile.contains("tile_flag")) && !boardArray[i]) tile.add("tile_cross");
+    }
+    inGame = false;
+    clearInterval(timer);
+}
+
+function openAll() {
+    for(let i = 1; i <= numCells; i++) openTile(`tile-${i}`);
+    inGame = false;
+    clearInterval(timer);
+    isWin = true;
+}
+
+
+function countTile(cell) {
+    let res = 0;
+    mark(cell, "count");
+    document.querySelectorAll(".count").forEach((x) => {
+        if(boardArray[x.classList[1].split('-')[1]]) res++;
+        x.classList.remove("count");
+    });
+    if(res) {
+        document.querySelector(`.tile-${cell}`).classList.add(`number_${res}`);
+        document.querySelector(`.tile-${cell}`).textContent = res;
+    } else {
+        mark(cell, "mark");
+        document.querySelectorAll(".mark").forEach((x) => {
+            if(!x.classList.contains("tile_open")) openTile(x.classList[1]);
+        });
+    }
+}
+
+function mark(cell, className) {
+    let rowCol = Math.sqrt(numCells);
+    if((cell - 1) % rowCol) document.getElementById(`tile-${cell-1}`).classList.add(className);             //left
+    if(cell % rowCol) document.getElementById(`tile-${cell+1}`).classList.add(className);                   //right
+    if(cell > rowCol) {                                                                          //**if not 1st row
+        document.getElementById(`tile-${cell-rowCol}`).classList.add(className);                            //up
+        if((cell - 1) % rowCol) document.getElementById(`tile-${cell-rowCol-1}`).classList.add(className);  //up-left if not 1st col
+        if(cell % rowCol) document.getElementById(`tile-${cell-rowCol+1}`).classList.add(className);        //up-right if not last col
+    }
+    if(cell <= numCells - rowCol) {                                                              //**if not last row
+        document.getElementById(`tile-${cell+rowCol}`).classList.add(className);                            // down
+        if((cell - 1) % rowCol) document.getElementById(`tile-${cell+rowCol-1}`).classList.add(className);  //down-left if not 1st col
+        if(cell % rowCol) document.getElementById(`tile-${cell+rowCol+1}`).classList.add(className);        //down-right if not last col
+    }
+}
+
+function flagTile(num) {
+    let tile = document.querySelector(`.${num}`).classList;
+    if(!tile.contains("tile_open")) {
+        if(tile.contains("tile_flag")) {
+            flags++;
+            tile.remove("tile_flag");
+            document.querySelector(".flag").textContent = flags;
+        } else {
+            flags--;
+            tile.add("tile_flag");
+            document.querySelector(".flag").textContent = flags;
+        }
+        if(flags === 0) {
+            let win = true;
+            document.querySelectorAll(".tile_flag").forEach((x) => {
+                if(!boardArray[x.classList[1].split('-')[1]]) win = false;
+            });
+            if(win) openAll(); else openBombs(0);
+        }
+    }
+}
+
 document.addEventListener("click", (event) => {
     if(!event.target.classList.contains("menu") &&
         !event.target.classList.contains("header__butt-container") &&
@@ -95,4 +207,8 @@ document.addEventListener("click", (event) => {
     if(event.target.classList.contains("menu__radio") || event.target.classList.contains("menu__checkbox")) newGame();
     if(inGame) if(event.target.classList.contains("tile")) openTile(event.target.classList[1]);
     console.log(event.target.classList);
+});
+window.addEventListener("contextmenu", (event) => {
+    event.preventDefault();
+    if(inGame) if(event.target.classList.contains("tile")) flagTile(event.target.classList[1]);
 });
